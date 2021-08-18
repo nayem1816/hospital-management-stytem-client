0b9c6b682e167f7754468bf0cf0d08ea8ee00ca0
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-08-18 18:15:06 +0600

    demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h2>Hospital Management System</h2>
+        </div>
+    );
 }
 
 export default App;
