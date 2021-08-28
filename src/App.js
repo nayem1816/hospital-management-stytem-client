c1235edf70bc6a634b0fa0aea2bddce859354f83
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-08-28 21:32:05 +0600

    sidebar topbar and switch route sidebar options

--- src/App.js
+++ src/App.js
@@ -2,24 +2,62 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import NotFound from "./components/NotFound/NotFound";
+import AddDoctor from "./pages/Doctor/AddDoctor/AddDoctor";
+import { CssBaseline } from "@material-ui/core";
+import Topbar from "./components/Topbar/Topbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import clsx from "clsx";
+import { useState } from "react";
+import { useStyles } from "./basic-style/App.style";
 
 function App() {
+    const classes = useStyles();
+    const [open, setOpen] = useState(true);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
     return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/">
-                        <Dashboard />
-                    </Route>
-                    <Route path="/dashboard">
-                        <Dashboard />
-                    </Route>
-                    <Route path="*">
-                        <NotFound />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <div className="">
+            <Router>
+                <div className={classes.root}>
+                    <CssBaseline />
+                    <Topbar
+                        open={open}
+                        handleDrawerOpen={handleDrawerOpen}
+                        classes={classes}
+                    />
+                    <Sidebar
+                        handleDrawerClose={handleDrawerClose}
+                        open={open}
+                        classes={classes}
+                    />
+                    <main
+                        className={clsx(classes.content, {
+                            [classes.contentShift]: open,
+                        })}
+                    >
+                        <div className={classes.drawerHeader} />
+                        <Switch>
+                            <Route exact path="/">
+                                <Dashboard />
+                            </Route>
+                            <Route path="/dashboard">
+                                <Dashboard />
+                            </Route>
+                            <Route path="/doctor/addDoctor">
+                                <AddDoctor />
+                            </Route>
+                            <Route path="*">
+                                <NotFound />
+                            </Route>
+                        </Switch>
+                    </main>
+                </div>
+            </Router>
+        </div>
     );
 }
 
