052296c3bb614a2ff62b3fb1419db8604b134e0f
imsamrat <imsamrat24@gmail.com>
2021-08-30 20:06:10 +0600

    Add some text fill

--- src/pages/BedManager/AddBed/AddBed.jsx
+++ src/pages/BedManager/AddBed/AddBed.jsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import CustomInput from "./../../../components/InputField/CustomInput/CustomInput";
+import { useForm } from "react-hook-form";
+import CustomButton from "../../../components/InputField/CustomButton/CustomButton";
+import CustomSelect from "../../../components/InputField/CustomSelect/CustomSelect";
+import UploadField from "../../../components/InputField/UploadField/UploadField";
 
 const AddBed = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <h2>Add Bed</h2>
+      <div className="d-flex justify-content-between">
+        <h4>Add Bed</h4>
+        <h4>
+          <Link to="/bedManager/addBed">Bed List</Link>
+        </h4>
+      </div>
+      <div className="mt-3">
+        <form className="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <div className="col-md-6 p-3 first-name">
+              <CustomInput
+                placeHolder={"First Name"}
+                inputType={"text"}
+                {...register("firstName", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 last-name">
+              <CustomInput
+                placeHolder={"Last Name"}
+                inputType={"text"}
+                {...register("lastName", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 email">
+              <CustomInput
+                placeHolder={"Room No"}
+                inputType={"number"}
+                {...register("roomNo", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 mobile">
+              <CustomInput
+                placeHolder={"Bed Type"}
+                inputType={"text"}
+                {...register("bedType", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 mobile">
+              <CustomInput
+                placeHolder={"Admit Date"}
+                inputType={"text"}
+                {...register("admitDate", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 mobile">
+              <CustomInput
+                placeHolder={"Bed Capacity"}
+                inputType={"number"}
+                {...register("bedCapacity", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 address">
+              <CustomInput
+                placeHolder={"Description"}
+                inputType={"text"}
+                {...register("description", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 department">
+              <CustomSelect
+                placeHolder="Sex"
+                selectOptions={[
+                  "Male",
+                  "Female",
+                  "Others",
+                ]}
+                register={register}
+              />
+            </div>
+            <div className="col-md-6 p-3 mobile">
+              <UploadField />
+            </div>
+            <div className="col-md-12 p-3 button">
+              <CustomButton
+                btnType={"btn"}
+                btnTxt={"Reset"}
+                color="secondary"
+              />
+              <CustomButton btnType={"submit"} btnTxt={"Submit"} />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/HumanResource/AddRepresentative/AddRepresentative.jsx
+++ src/pages/HumanResource/AddRepresentative/AddRepresentative.jsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import CustomPassInput from "../../../components/InputField/CustomPassInput/CustomPassInput";
+import CustomInput from "./../../../components/InputField/CustomInput/CustomInput";
+import { useForm } from "react-hook-form";
+import CustomButton from "../../../components/InputField/CustomButton/CustomButton";
+import CustomSelect from "../../../components/InputField/CustomSelect/CustomSelect";
+import UploadField from "../../../components/InputField/UploadField/UploadField";
 
 const AddRepresentative = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <h2>Add Representative</h2>
+      <div className="d-flex justify-content-between">
+        <h4>Add Representative</h4>
+        <h4>
+          <Link to="/humanResource/addRepresentative">Representative List</Link>
+        </h4>
+      </div>
+      <div className="mt-3">
+        <form className="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <div className="col-md-6 p-3 first-name">
+              <CustomInput
+                placeHolder={"First Name"}
+                inputType={"text"}
+                {...register("firstName", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 last-name">
+              <CustomInput
+                placeHolder={"Last Name"}
+                inputType={"text"}
+                {...register("lastName", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 email">
+              <CustomInput
+                placeHolder={"Email"}
+                inputType={"email"}
+                {...register("email", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 password">
+              <CustomPassInput register={register} />
+            </div>
+            <div className="col-md-6 p-3 mobile">
+              <CustomInput
+                placeHolder={"Mobile"}
+                inputType={"number"}
+                {...register("mobile", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 address">
+              <CustomInput
+                placeHolder={"Address"}
+                inputType={"text"}
+                {...register("address", { required: true })}
+              />
+            </div>
+            <div className="col-md-6 p-3 department">
+              <CustomSelect
+                placeHolder="Sex"
+                selectOptions={[
+                  "Male",
+                  "Female",
+                  "Others",
+                ]}
+                register={register}
+              />
+            </div>
+            <div className="col-md-6 p-3 department">
+              <CustomSelect
+                placeHolder="Status"
+                selectOptions={[
+                  "Active",
+                  "Inactive",
+                ]}
+                register={register}
+              />
+            </div>
+            <div className="col-md-6 p-3 mobile">
+              <UploadField />
+            </div>
+            <div className="col-md-12 p-3 button">
+              <CustomButton
+                btnType={"btn"}
+                btnTxt={"Reset"}
+                color="secondary"
+              />
+              <CustomButton btnType={"submit"} btnTxt={"Submit"} />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
